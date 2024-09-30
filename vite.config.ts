10fd8143f65cca9fcc3a644e81a1021b91162c3b
MiladSedhom <MiladSedhom@gmail.com>
2024-09-30 17:45:45 +0300

    feat: setup unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import Unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), Unocss()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
