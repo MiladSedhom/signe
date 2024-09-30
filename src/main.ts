022a2d06ac136305577a8130fe41e7543c1a53f0
MiladSedhom <MiladSedhom@gmail.com>
2024-09-30 17:53:39 +0300

    feat: style reset

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import 'virtual:uno.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'virtual:uno.css'
+import '@unocss/reset/tailwind.css'
 
 const app = createApp(App)
 
